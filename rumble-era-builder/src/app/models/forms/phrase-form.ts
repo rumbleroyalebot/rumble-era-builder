1be6976822be5f5fc69345af1092c4a5149bf24f
milan_25 <milan.abraham2@seznam.cz>
2023-10-14 13:09:38 +0200

    Phrase length limit

--- rumble-era-builder/src/app/models/forms/phrase-form.ts
+++ rumble-era-builder/src/app/models/forms/phrase-form.ts
@@ -1,7 +1,8 @@
-import { prop, required } from "@rxweb/reactive-form-validators";
+import { maxLength, prop, required } from "@rxweb/reactive-form-validators";
 
 export class PhraseForm {
     @prop()
     @required({message: "Phrase cannot be empty."})
+    @maxLength({value: 200, message: "Phrase cannot be longer than 200 characters."})
     phrase = "";
 }
