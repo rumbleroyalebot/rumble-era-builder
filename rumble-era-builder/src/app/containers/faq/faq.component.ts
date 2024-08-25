3587584c7c6057aefe560802ab34a2c012e59182
Mohammed Khaled <48189737+MoKhaled112@users.noreply.github.com>
2024-08-25 18:52:20 +0300

    Update faq.component.ts

--- rumble-era-builder/src/app/containers/faq/faq.component.ts
+++ rumble-era-builder/src/app/containers/faq/faq.component.ts
@@ -1,11 +1,40 @@
-import { Component } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: "app-faq",
-  standalone: true,
-  imports: [RouterModule],
-  templateUrl: "./faq.component.html",
-  styleUrl: "./faq.component.less",
+  selector: 'app-faq',
+  templateUrl: './faq.component.html',
+  styleUrls: ['./faq.component.less'],
+  imports:[RouterModule]
 })
-export class FaqComponent {}
+export class FaqComponent implements OnInit {
+
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+
+  ngOnInit() {
+    this.renderer.listen('document', 'DOMContentLoaded', () => {
+      const questionElements = this.el.nativeElement.querySelectorAll('.question');
+
+      questionElements.forEach((question: HTMLElement) => {
+        this.renderer.listen(question, 'click', () => {
+          const answer = question.nextElementSibling as HTMLElement;
+          const icon = question.querySelector('.icon') as HTMLElement;
+
+          question.closest('.faq-item')?.classList.toggle('active');
+
+          if (answer.style.maxHeight) {
+            answer.style.maxHeight = '';
+            if (icon) {
+              icon.style.transform = 'rotate(0deg)';
+            }
+          } else {
+            answer.style.maxHeight = `${answer.scrollHeight}px`;
+            if (icon) {
+              icon.style.transform = 'rotate(180deg)';
+            }
+          }
+        });
+      });
+    });
+  }
+}
